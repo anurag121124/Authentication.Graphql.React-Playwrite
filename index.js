475726dc9c7970419f0a3d9890ba9307541f5b6c
Anurag Singh <anurag.singh@fuelbuddy.in>
2024-05-24 16:32:04 +0530

    Backend

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-const { ApolloServer }  = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
-
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
+require('dotenv').config();
 
-const MONGODB = "mongodb+srv://cooper:codes@cluster0.isxsidq.mongodb.net/?retryWrites=true&w=majority";
+const MONGODB = "mongodb://localhost:27017/mydatabas" ;
+const PORT = process.env.PORT || 4000;
+
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({ req }) => ({ req })
+    context: ({ req }) => ({ req }),
 });
 
-mongoose.connect(MONGODB, {useNewUrlParser: true})
+mongoose.set("strictQuery", false);
+
+mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("MongoDB Connected");
-        return server.listen({port: process.env.PORT || 4000});
+        return server.listen({ port: PORT });
     })
     .then((res) => {
-        console.log(`Server running at ${res.url}`)
+        console.log(`Server running at ${res.url}`);
+    })
+    .catch(err => {
+        console.error("Error connecting to MongoDB or starting server:", err);
     });
